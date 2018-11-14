fafb995fce49a94cfff8920df22ac5532068e7d3
Guilhermx <2cardosoguilherme2@Gmail.com>
2018-11-13 22:40:09 -0200

    Exercicios 401 e 402

---
+++ ListaExerciciosLevel2/src/exercicio401.java
@@ -0,0 +1,44 @@
+import static javax.swing.JOptionPane.showInputDialog; //input
+import static javax.swing.JOptionPane.showMessageDialog; //output
+import static java.lang.Integer.parseInt; //String -> int
+import static java.lang.String.valueOf; // ?? -> String
+import java.util.*;
+
+public class exercicio401{
+    public static void main(String[] args) {
+        int qtdNumeros = 10;
+        int[] a = new int[qtdNumeros];
+        int[] b = new int[qtdNumeros];
+        String msg =  "Resultadop Final\n";
+                
+                
+        for(int cont = 0; cont < a.length; cont++){
+        a[cont] = receberInt(
+           "Digite o " + (cont + 1) + " º número"            
+        );
+        
+        b[cont] = cont % 2 == 0?
+                a[cont] * 5
+                :
+                a[cont] + 5;
+        msg +=
+                "A[" + cont + "]: " +a[cont] + "     " +
+                "B[" + cont + "]: " +b[cont] + "\n";     
+     }
+    
+     
+    }
+       public static int receberInt(String mensagemProUsuario) {
+        String aux = showInputDialog(mensagemProUsuario);
+        try {
+            return parseInt(aux);
+        } catch (Exception e) {
+            exibir("Você digitou algo errado :(");
+            return 0;
+        }
+    }
+ 
+       public static void exibir(String mensagemProUsuario) {
+        showMessageDialog(null, mensagemProUsuario);
+    }
+}
